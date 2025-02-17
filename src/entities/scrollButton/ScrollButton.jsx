18316fc507d76bb1ebe2752b1b204c9e5f5f0c6e
Sultanbek02173 <sultan.02173@gmail.com>
2025-02-17 15:20:15 +0600

    FIX bugs

--- src/entities/scrollButton/ScrollButton.jsx
+++ src/entities/scrollButton/ScrollButton.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './ScrollButton.scss';
+import './scrollButton.scss';
 import scrollButton from './ScrollButton.svg';
 
 export function ScrollButton() {
